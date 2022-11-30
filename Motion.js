70ecc3ce3411b967495a0c1bd2cf34888c773202
devonkay223 <devonkay223@gmail.com>
2022-11-29 16:33:31 -0800

    add osc

--- Motion.js
+++ Motion.js
@@ -1,5 +1,22 @@
 var x, y, z;
 
+webSocketPort = new osc.WebSocketPort({
+  socket: ws
+});
+
+// send message
+maxAPI.addHandler("send", (...args) => {
+  if (webSocketPort && isConnected) {
+      webSocketPort.send({
+              address: "/max/midi",
+              args: [{
+                      type: "i",
+                      value: args[0]
+              }]
+          });
+  }
+});
+
 window.addEventListener('devicemotion', function(e) 
 { 
   x = parseFloat(e.acceleration.x).toFixed(3);
@@ -7,11 +24,18 @@ window.addEventListener('devicemotion', function(e)
   z = parseFloat(e.acceleration.z).toFixed(3); 
 
   console.log("(", x, ", ", y, ", ", z, ") \n");
+
+  var message = new OSC.Message('/test/random', Math.random());
+  osc.send(message);
 });
 
 function getPerm(){
   DeviceMotionEvent.requestPermission()
 }
+
+
+
+
 
 
 
