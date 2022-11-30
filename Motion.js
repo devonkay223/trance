62952b105b6380ac6f31d7b644b0bbed7790713a
devonkay223 <devonkay223@gmail.com>
2022-11-29 16:06:31 -0800

    add logging

--- Motion.js
+++ Motion.js
@@ -5,6 +5,8 @@ window.addEventListener('devicemotion', function(e)
   x = parseFloat(e.acceleration.x).toFixed(3);
   y = parseFloat(e.acceleration.y).toFixed(3);
   z = parseFloat(e.acceleration.z).toFixed(3); 
+
+  console.log("(", x, ", ", y, ", ", z, ") \n");
 });
 
 function getPerm(){
